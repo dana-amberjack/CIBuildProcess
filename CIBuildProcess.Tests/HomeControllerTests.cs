248757bcc68da35281021f1a8f46cc17bcf92f51
Dana Nurse <dana.nurse@weareamberjack.com>
2019-05-15 14:45:40 +0100

    Add Tests

---
+++ CIBuildProcess.Tests/HomeControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Mvc;
+using CIBuildProcess.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CIBuildProcess.Tests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void AboutReturnsActionResult()
+        {
+            var controller = new HomeController();
+
+            var result = controller.About();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ActionResult));
+        }
+    }
+}
---
+++ CIBuildProcess.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CIBuildProcess.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CIBuildProcess.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e35d9ff3-6413-47ff-a200-6023b65da2da")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
